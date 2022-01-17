8dc3b8a0277175fad2ff69e398c783673996c30f
Michael <mbahl1670@gmail.com>
2022-01-17 14:44:56 -0600

    user routes added

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET ALL users route /api/users
+router.get('/', (req,res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// GET a SINGLE user route /api/users/:id
+router.get('/:id', (req,res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// POST create a User route /api/users
+router.post('/', (req,res) => {
+    User.create({
+        username: req.body.email,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// PUT change User info route /api/users/:id
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// DELETE a User route /api/users/:id
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO user
+  (username, email, password)
+VALUES
+  ("mbahl1670", "mbahl1670@gmail.com", "pass1234"),
+  ("mikbahl", "mikbahl@hotmail.com", "pass1234");
