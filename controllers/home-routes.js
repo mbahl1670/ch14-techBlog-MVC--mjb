659f126a17a6af630edc1d988adbf0b5a7a287ba
Michael <mbahl1670@gmail.com>
2022-01-21 21:54:06 -0600

    starting work on single posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const { route } = require('express/lib/application');
-const { Post } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     Post.findAll({
-        attributes: ['title', 'created_at']
+        attributes: ['title', 'created_at', 'id']
     })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain:true }));
@@ -24,6 +24,47 @@ router.get('/login', (req, res) => {
         return;
     }
     res.render('login');
+});
+
+router.get('/post/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['title', 'content', 'created_at'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['comment_text'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+        console.log(post);
+
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
