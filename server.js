a76952f2c4958988724caf1c5fd0daa2cdfa737d
Michael <mbahl1670@gmail.com>
2022-01-23 15:20:16 -0600

    cookie expires over time

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
-    // cookie: { 
-    //   expires: 600000 
-    // },
+    cookie: { 
+      expires: 600000 
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
